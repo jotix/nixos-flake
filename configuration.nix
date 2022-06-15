818b13a45469d35022207152db98ada39c80dcb6
jotix <jujodeve@gmail.com>
2022-06-15 15:50:40 -0300

    gnome

--- configuration.nix
+++ configuration.nix
@@ -43,10 +43,10 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
